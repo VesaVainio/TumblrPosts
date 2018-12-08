fea4a6bd9c9a83ce9e399c5a71c11fc019f8a1a0
Vesa Vainio <vesatvainio@gmail.com>
2018-12-08 21:45:45 +0200

    Package dependencies now mostly OK

---
+++ TableInterface/TableAdapter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure; // Namespace for CloudConfigurationManager
+using Microsoft.Azure.Storage; // Namespace for StorageAccounts
+using Microsoft.Azure.CosmosDB.Table; // Namespace for Table storage types
+
+namespace TableInterface
+{
+    public class TableAdapter
+    {
+        public void Init()
+        {
+            // Retrieve the storage account from the connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            // Create the table client.
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+
+            // Retrieve a reference to the table.
+            CloudTable table = tableClient.GetTableReference("people");
+
+            // Create the table if it doesn't exist.
+            table.CreateIfNotExists();
+        }
+    }
+}
