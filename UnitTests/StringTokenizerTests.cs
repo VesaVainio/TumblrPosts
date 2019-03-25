063bf0637fc4b5021f42840cd35695bba4985a13
Vesa Vainio <vesatvainio@gmail.com>
2019-03-25 21:49:06 +0200

    Tidy up test text

--- UnitTests/StringTokenizerTests.cs
+++ UnitTests/StringTokenizerTests.cs
@@ -11,18 +11,18 @@ namespace UnitTests
         public void TestStringTokenizer1()
         {
             string[] tokens = StringTokenizer.Tokenize(
-                "Why Do I Tease & Deny\nYou And Give You\nA Raging Hardon\nEveryday ???\nBecause It Builds\nYour Libido My\nStudmuffin...\n" +
-                "I'm Not Only Training\nYour Erection How\nNOT to Cum,\nI'm Also Programming\nYour Mind My Darling\nWith Dopamine...\n");
+                "Why Do I Tease & Deny\nYou And Give You\nA Hard Time\nEveryday ???\nBecause It Builds\nYour Character My\nStudmuffin...\n" +
+                "I'm Not Only Training\nYour Mind How\nNOT to Complain,\nI'm Also Programming\nYour Mind My Darling\nWith Dopamine...\n");
 
             Assert.AreEqual(39, tokens.Length);
-            Assert.IsTrue(tokens.Any(x => x.Equals("I'm")));
+            Assert.IsTrue(tokens.Any(x => x.Equals("i'm")));
         }
 
         [TestMethod]
         public void TestStringTokenizer2()
         {
             string[] tokens = StringTokenizer.Tokenize(
-                "Thanks for the lotion!)\nNow you can have 5 minutes to find your key\nin the pool unlock your belt and cum.Better hurryup!\n");
+                "Thanks for the lotion!)\nNow you can have 5 minutes to find your key\nin the pool unlock your device and play.Better hurryup!\n");
 
             Assert.AreEqual(24, tokens.Length);
             Assert.IsTrue(tokens.All(x => !x.Contains("!")));
