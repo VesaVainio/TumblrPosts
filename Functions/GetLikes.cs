1dd272dbeb08efada1427e32920fb0d43c269c58
Vesa Vainio <vesatvainio@gmail.com>
2019-03-26 23:11:34 +0200

    Made GetLikes get only the new likes

--- Functions/GetLikes.cs
+++ Functions/GetLikes.cs
@@ -1,12 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
 using QueueInterface;
-using System;
-using System.Configuration;
-using System.Net;
-using System.Net.Http;
-using System.Threading.Tasks;
+using TableInterface;
 using TumblrPics.Model.Tumblr;
 
 namespace Functions
@@ -14,7 +17,8 @@ namespace Functions
     public static class GetLikes
     {
         [FunctionName("GetLikes")]
-        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = "getlikes/{blogname}")]HttpRequestMessage req,
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = "getlikes/{blogname}")]
+            HttpRequestMessage req,
             string blogname, TraceWriter log)
         {
             Startup.Init();
@@ -23,6 +27,11 @@ namespace Functions
 
             PostsToProcessQueueAdapter postsToProcessQueueAdapter = new PostsToProcessQueueAdapter();
             postsToProcessQueueAdapter.Init(log);
+
+            LikeIndexTableAdapter likeIndexTableAdapter = new LikeIndexTableAdapter();
+            likeIndexTableAdapter.Init();
+
+            long newestLikedTimestamp = likeIndexTableAdapter.GetNewestLikedTimestamp(blogname);
 
             Likes likes = null;
             long totalCount = 0;
@@ -42,12 +51,26 @@ namespace Functions
                     {
                         url = "https://api.tumblr.com/" + likes._links.Next.Href + "&api_key=" + apiKey;
                     }
+
                     log.Info("Making request to: " + url);
                     HttpResponseMessage response = await httpClient.GetAsync(url);
                     if (response.IsSuccessStatusCode)
                     {
                         TumblrResponse<Likes> tumblrResponse = await response.Content.ReadAsAsync<TumblrResponse<Likes>>();
                         likes = tumblrResponse.Response;
+
+                        if (newestLikedTimestamp > 0)
+                        {
+                            List<Post> newerPosts = likes.Liked_posts.Where(x => x.Liked_Timestamp > newestLikedTimestamp).ToList();
+                            if (newerPosts.Count < likes.Liked_posts.Count)
+                            {
+                                log.Info($"Reached Liked_Timestamp of {newestLikedTimestamp} which has already been fetched, finishing");
+                                postsToProcessQueueAdapter.SendPostsToProcess(newerPosts, blogname);
+
+                                totalCount += newerPosts.Count;
+                                break;
+                            }
+                        }
 
                         totalCount += likes.Liked_posts.Count;
 
@@ -58,7 +81,6 @@ namespace Functions
                         log.Info("Got response: " + response.ReasonPhrase + " " + response.StatusCode);
                         break;
                     }
-
                 } while (likes._links != null && likes._links.Next != null && !string.IsNullOrEmpty(likes._links.Next.Href));
             }
 
--- TableInterface/LikeIndexTableAdapter.cs
+++ TableInterface/LikeIndexTableAdapter.cs
@@ -41,5 +41,19 @@ namespace TableInterface
             List<LikeIndexEntity> entities = result.ToList();
             return entities;
         }
+
+        public long GetNewestLikedTimestamp(string blogName)
+        {
+            string pkFilter = TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, blogName);
+            TableQuery query = new TableQuery().Where(pkFilter).Select(new List<string> { "LikedTimestamp" });
+            IEnumerable<DynamicTableEntity> result = likeIndexTable.ExecuteQuery(query);
+            DynamicTableEntity lastEntity = result.LastOrDefault();
+            if (lastEntity != null)
+            {
+                return Convert.ToInt64(lastEntity.Properties["LikedTimestamp"].StringValue);
+            }
+
+            return -1;
+        }
     }
 }
