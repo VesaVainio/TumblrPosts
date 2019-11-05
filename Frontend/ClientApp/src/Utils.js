b28813eee9902c55eaf7efce370218d7bf93e0b8
Vesa Vainio <vesatvainio@gmail.com>
2019-11-05 23:50:16 +0200

    Increased photo posts' thumbnail size

--- Frontend/ClientApp/src/Utils.js
+++ Frontend/ClientApp/src/Utils.js
@@ -1,20 +1,23 @@
 const Utils = {
-    GetSmallPhotoUrl: function(post) {
-        let photo = post.Photos[0];
-        photo.Sizes.sort((a, b) => a.Nominal - b.Nominal);
-        let size = photo.Sizes[0];
-        let base = process.env.REACT_APP_BLOB_ROOT;
-        let url = base + "/" + size.Container + "/" + photo.Name + "_" + size.Nominal + "." + photo.Extension;
-        return url;
-    },
-
-    GetBigPhotoUrl: function (photo) {
-        photo.Sizes.sort((a, b) => b.Nominal - a.Nominal);
-        let size = photo.Sizes[0];
-        let base = process.env.REACT_APP_BLOB_ROOT;
-        let url = base + "/" + size.Container + "/" + photo.Name + "_" + size.Nominal + "." + photo.Extension;
-        return url;
+  GetSmallPhotoUrl: function(post) {
+    const photo = post.Photos[0];
+    photo.Sizes.sort((a, b) => a.Nominal - b.Nominal);
+    let size = photo.Sizes[0];
+    if (size.Nominal === 250 && photo.Sizes.length > 1) {
+      size = photo.Sizes[1];
     }
-}
+    const base = process.env.REACT_APP_BLOB_ROOT;
+    const url = base + "/" + size.Container + "/" + photo.Name + "_" + size.Nominal + "." + photo.Extension;
+    return url;
+  },
+
+  GetBigPhotoUrl: function(photo) {
+    photo.Sizes.sort((a, b) => b.Nominal - a.Nominal);
+    let size = photo.Sizes[0];
+    const base = process.env.REACT_APP_BLOB_ROOT;
+    const url = base + "/" + size.Container + "/" + photo.Name + "_" + size.Nominal + "." + photo.Extension;
+    return url;
+  }
+};
 
 export default Utils;
