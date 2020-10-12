65c419c1221a11618458f88f9e509997f6de1506
Vesa Vainio <vesatvainio@gmail.com>
2020-10-12 22:08:53 +0300

    Better logging for InitIndexing

--- Functions/InitIndexing.cs
+++ Functions/InitIndexing.cs
@@ -25,14 +25,21 @@ namespace Functions
             blogToIndexQueueAdapter.Init();
 
             List<string> partitions = postsTableAdapter.GetAllPartitions();
+            log.Info($"Got {partitions.Count} blogs to index");
 
             //Random random = new Random();
             //partitions.Shuffle(random);
             //partitions = partitions.Take(10).ToList();
 
+            int index = 0;
             foreach (string blogname in partitions)
             {
                 blogToIndexQueueAdapter.Send(new BlogToIndex { Blogname = blogname });
+                index++;
+                if (index % 100 == 0)
+                {
+                    log.Info($"Queued {index} blogs for indexing");
+                }
             }
 
             return req.CreateResponse(HttpStatusCode.OK, "Queued " + partitions.Count + " blogs for indexing");
